a1c5eb364f33bb753080260d02d48cd47b0e6d22
Te3Ka <Mnement4813@yandex.ru>
2024-09-13 22:20:20 +0300

    solution 2235 complete

---
+++ 2235/src/main/java/ru/te3ka/exercisesleetcode/Main.java
@@ -0,0 +1,8 @@
+package ru.te3ka.exercisesleetcode;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.sum(7, 6));
+    }
+}
---
+++ 2235/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
@@ -0,0 +1,7 @@
+package ru.te3ka.exercisesleetcode;
+
+public class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
