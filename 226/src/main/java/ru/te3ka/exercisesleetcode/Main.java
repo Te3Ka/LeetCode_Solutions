3f3a18da457b1d16326b1ffe39b0ede524d8d887
Te3Ka <Mnement4813@yandex.ru>
2024-09-16 09:02:34 +0300

    solution 226 -- Задача решена, но нужно её доделать косметически - toString и юнит-тесты

---
+++ 226/src/main/java/ru/te3ka/exercisesleetcode/Main.java
@@ -0,0 +1,41 @@
+package ru.te3ka.exercisesleetcode;
+
+public class Main {
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(
+                4,
+                new TreeNode(2,
+                        new TreeNode(1),
+                        new TreeNode(3)
+                ),
+                new TreeNode(7,
+                        new TreeNode(6),
+                        new TreeNode(9)
+                )
+        );
+
+        System.out.println("input:  [ " + treeNode.val + " "
+                + treeNode.left.val + " "
+                + treeNode.right.val + " "
+                + treeNode.left.left.val + " "
+                + treeNode.left.right.val + " "
+                + treeNode.right.left.val + " "
+                + treeNode.right.right.val + " ]"
+        );
+
+        System.out.println(treeNode);
+
+        Solution solution = new Solution();
+
+        TreeNode invertTree = solution.invertTree(treeNode);
+
+        System.out.println("output: [ " + invertTree.val + " "
+                + invertTree.left.val + " "
+                + invertTree.right.val + " "
+                + invertTree.left.left.val + " "
+                + invertTree.left.right.val + " "
+                + invertTree.right.left.val + " "
+                + invertTree.right.right.val + " ]"
+        );
+    }
+}
---
+++ 226/src/main/java/ru/te3ka/exercisesleetcode/TreeNode.java
@@ -0,0 +1,26 @@
+package ru.te3ka.exercisesleetcode;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    //TODO: Сделать toString
+    @Override
+    public String toString() {
+        return "";
+    }
+}
