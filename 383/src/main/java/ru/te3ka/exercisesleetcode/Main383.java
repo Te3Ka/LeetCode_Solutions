89da34618f0f87e858c84bac6c538090dfdc7414
Te3Ka <Mnement4813@yandex.ru>
2024-10-07 18:35:30 +0300

    solution 383 complete

--- 383/src/main/java/ru/te3ka/exercisesleetcode/Main383.java
+++ 383/src/main/java/ru/te3ka/exercisesleetcode/Main383.java
@@ -2,6 +2,14 @@ package ru.te3ka.exercisesleetcode;
 
 public class Main383 {
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        System.out.println(solution.canConstruct("a", "b"));
+        System.out.println(solution.canConstruct("a", "a"));
+        System.out.println(solution.canConstruct("aa", "ab"));
+        System.out.println(solution.canConstruct("aa", "aab"));
+        System.out.println(solution.canConstruct("inez", "magazine"));
+        System.out.println(solution.canConstruct("gaz", "magazine"));
+        System.out.println(solution.canConstruct("zagai", "magazine"));
+        System.out.println(solution.canConstruct("zaagai", "magazine"));
     }
 }
--- 383/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
+++ 383/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
@@ -1,7 +1,28 @@
 package ru.te3ka.exercisesleetcode;
 
+import java.util.HashMap;
+
 public class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        return false;
+        HashMap<Character, Integer> hashMapRansomNote = new HashMap<>();
+        HashMap<Character, Integer> hashMapMagazine = new HashMap<>();
+
+        for (char ransomNoteChar : ransomNote.toCharArray()) {
+            hashMapRansomNote.put(ransomNoteChar, hashMapRansomNote.getOrDefault(ransomNoteChar, 0) + 1);
+        }
+
+        for (char magazineChar : magazine.toCharArray()) {
+            hashMapMagazine.put(magazineChar, hashMapMagazine.getOrDefault(magazineChar, 0) + 1);
+        }
+
+        for (char ransomNoteChar : hashMapRansomNote.keySet()) {
+            if (!hashMapMagazine.containsKey(ransomNoteChar) ||
+                    !(hashMapRansomNote.get(ransomNoteChar) <= hashMapMagazine.get(ransomNoteChar))
+            ) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
