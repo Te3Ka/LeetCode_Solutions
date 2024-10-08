25aea5f1683dee31acb11519205903818c85b85d
Te3Ka <Mnement4813@yandex.ru>
2024-10-08 18:36:25 +0300

    solution 485 complete

--- 485/src/main/java/ru/te3ka/exercisesleetcode/Main485.java
+++ 485/src/main/java/ru/te3ka/exercisesleetcode/Main485.java
@@ -3,6 +3,11 @@ package ru.te3ka.exercisesleetcode;
 
 public class Main485 {
     public static void main(String[] args) {
+//        int[] arrayTest = {1, 1, 0, 0, 1, 1, 1};
+        int[] arrayTest = {0, 0, 1, 1, 0, 0, 1};
 
+        Solution solution = new Solution();
+
+        System.out.println(solution.findMaxConsecutiveOnes(arrayTest));
     }
 }
--- 485/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
+++ 485/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
@@ -2,6 +2,15 @@ package ru.te3ka.exercisesleetcode;
 
 public class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
-
+        int countOnes = 0, maxCountOnes = 0;
+        for (int num : nums) {
+            if (num == 1) {
+                countOnes++;
+                if (countOnes > maxCountOnes)
+                    maxCountOnes = countOnes;
+            } else
+                countOnes = 0;
+        }
+        return maxCountOnes;
     }
 }
