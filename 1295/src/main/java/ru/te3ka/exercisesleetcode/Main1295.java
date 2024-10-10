1a56c88fe9a02b8ce1072bd4cc269270a934832f
Te3Ka <Mnement4813@yandex.ru>
2024-10-10 13:00:44 +0300

    solution 1295 complete

--- 1295/src/main/java/ru/te3ka/exercisesleetcode/Main1295.java
+++ 1295/src/main/java/ru/te3ka/exercisesleetcode/Main1295.java
@@ -2,6 +2,11 @@ package ru.te3ka.exercisesleetcode;
 
 public class Main1295 {
     public static void main(String[] args) {
+//        int[] nums = {12,345,2,6,7896};
+//        int[] nums = {555, 901, 482, 1771};
+        int[] nums = {5055, 9001, 4082, 10771};
         Solution solution = new Solution();
+
+        System.out.println(solution.findNumbers(nums));
     }
 }
--- 1295/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
+++ 1295/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
@@ -2,6 +2,17 @@ package ru.te3ka.exercisesleetcode;
 
 public class Solution {
     public int findNumbers(int[] nums) {
-        return 0;
+        int countEvenNumbers = 0;
+
+        for (int num : nums) {
+            int countDivided = 0;
+            while (num > 0) {
+                countDivided++;
+                num /= 10;
+            }
+            if (countDivided % 2 == 0)
+                countEvenNumbers++;
+        }
+        return countEvenNumbers;
     }
 }
