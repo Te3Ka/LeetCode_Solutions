67fa5ad9325ba5147f6819fa91b6244f162d0377
Te3Ka <Mnement4813@yandex.ru>
2024-10-10 16:20:06 +0300

    solution 977 complete -- решение через новый массив и его сортировку. -- Есть другое решение.

--- 977/src/main/java/ru/te3ka/exercisesleetcode/Main977.java
+++ 977/src/main/java/ru/te3ka/exercisesleetcode/Main977.java
@@ -1,7 +1,16 @@
 package ru.te3ka.exercisesleetcode;
 
+import java.util.Arrays;
+
 public class Main977 {
     public static void main(String[] args) {
+//        int[] nums = {-4, -1, 0, 3, 10};
+//        int[] nums = {-7, -3, 2, 3, 11};
+//        int[] nums = {-7, -3, -2, 3, 11};
+        int[] nums = {-30, -11, -10, -7, -3, -2, 3, 11, 15, 29};
 
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(nums));
+        System.out.println(Arrays.toString(solution.sortedSquares(nums)));
     }
 }
--- 977/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
+++ 977/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
@@ -2,6 +2,20 @@ package ru.te3ka.exercisesleetcode;
 
 public class Solution {
     public int[] sortedSquares(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] *= nums[i];
+        }
 
+        int temp = 0;
+        for (int j = 0; j < nums.length - 1; j++) {
+            for (int i = 1; i < nums.length - j; i++) {
+                if (nums[i] < nums[i - 1]) {
+                    temp = nums[i];
+                    nums[i] = nums[i - 1];
+                    nums[i - 1] = temp;
+                }
+            }
+        }
+        return nums;
     }
 }
