40c6bd3329255f359b6ee53cf5066e2201df0295
Te3Ka <Mnement4813@yandex.ru>
2024-10-21 08:05:33 +0300

    solution 1089 complete

--- 1089/src/main/java/ru/te3ka/exercisesleetcode/Main1089.java
+++ 1089/src/main/java/ru/te3ka/exercisesleetcode/Main1089.java
@@ -4,7 +4,10 @@ import java.util.Arrays;
 
 public class Main1089 {
     public static void main(String[] args) {
-        int[] nums = {1, 0, 2, 3, 0, 4, 5, 0};
+//        int[] nums = {1, 0, 2, 3, 0, 4, 5, 0};
+        int[] nums = {9, 0, 0, 3, 3, 4, 5, 0};
+//        int[] nums = {1, 0, 2, 3, 0, 4, 5, 0};
+//        int[] nums = {1, 2, 3};
 
         Solution solution = new Solution();
 
--- 1089/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
+++ 1089/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
@@ -1,7 +1,20 @@
 package ru.te3ka.exercisesleetcode;
 
+import java.util.Arrays;
+
 public class Solution {
     public void duplicateZeros(int[] arr) {
-
+        int tempOne, tempTwo;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                tempOne = arr[i];
+                for (int j = i + 1; j < arr.length; j++) {
+                    tempTwo = arr[j];
+                    arr[j] = tempOne;
+                    tempOne = tempTwo;
+                }
+                i++;
+            }
+        }
     }
 }
