9061d474dd94b0405ffdc4106e2e42725bb8c4d4
Te3Ka <Mnement4813@yandex.ru>
2025-01-10 09:59:50 +0300

    solution 27 complete

--- 27/src/main/java/ru/te3ka/exercisesleetcode/Main27.java
+++ 27/src/main/java/ru/te3ka/exercisesleetcode/Main27.java
@@ -1,7 +1,19 @@
 package ru.te3ka.exercisesleetcode;
 
+import java.util.Arrays;
+
 public class Main27 {
     public static void main(String[] args) {
+//        int[] nums = {3, 2, 2, 3};
+//        int val = 3;
+
+        int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
+        int val = 2;
+
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(nums));
+        System.out.println(solution.removeElement(nums, val));
+        System.out.println(Arrays.toString(nums));
 
     }
 }
--- 27/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
+++ 27/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
@@ -2,6 +2,18 @@ package ru.te3ka.exercisesleetcode;
 
 public class Solution {
     public int removeElement(int[] nums, int val) {
+        int k = 0;
 
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        for (int i = k; i < nums.length; i++)
+            nums[i] = -1;
+
+        return k;
     }
 }
