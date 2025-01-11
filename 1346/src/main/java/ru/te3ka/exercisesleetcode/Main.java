3553cd9a2c0dcacb9c49cafbcb29a235b69fd8cb
Te3Ka <Mnement4813@yandex.ru>
2025-01-11 12:02:41 +0300

    solution 1346 start

---
+++ 1346/src/main/java/ru/te3ka/exercisesleetcode/Main.java
@@ -0,0 +1,11 @@
+package ru.te3ka.exercisesleetcode;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {10, 2, 5, 3};
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
