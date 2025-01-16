3dbf91a6014b1eb1520cb86715da560a6c7be7ef
Te3Ka <Mnement4813@yandex.ru>
2025-01-16 10:09:01 +0300

    solution 941 re-complete

--- 941/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
+++ 941/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
@@ -2,32 +2,20 @@ package ru.te3ka.exercisesleetcode;
 
 public class Solution {
     public boolean validMountainArray(int[] arr) {
-        int i = 1;
-        boolean isUp = true;
-        boolean isDown = false;
-        if (i >= arr.length)
+        if (arr.length < 3)
             return false;
-        while (isUp) {
-            if (i + 1 >= arr.length)
-                return false;
-            if (arr[i] > arr[i - 1] && arr[i] < arr[i + 1])
-                i++;
-            else if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
-                isUp = false;
-                isDown = true;
-                i++;
-            }
-            else
-                return false;
-        }
 
-        while (isDown) {
-            if (i + 1 >= arr.length)
-                return true;
-            if (arr[i] < arr[i - 1] && arr[i] > arr[i + 1])
-                i++;
-            else return false;
-        }
-        return false;
+        int i = 0;
+
+        while (i + 1 < arr.length && arr[i] < arr[i + 1])
+            i++;
+
+        if (i == 0 || i == arr.length - 1)
+            return false;
+
+        while (i + 1 < arr.length && arr[i] > arr[i + 1])
+            i++;
+
+        return i == arr.length - 1;
     }
 }
