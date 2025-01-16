e002c19e719080fde340eca6df0f2e7257fb655c
Te3Ka <Mnement4813@yandex.ru>
2025-01-16 10:03:09 +0300

    solution 941 complete

--- 941/src/main/java/ru/te3ka/exercisesleetcode/Main941.java
+++ 941/src/main/java/ru/te3ka/exercisesleetcode/Main941.java
@@ -1,7 +1,15 @@
 package ru.te3ka.exercisesleetcode;
 
+import java.util.Arrays;
+
 public class Main941 {
     public static void main(String[] args) {
+//        int[] arr = {0, 2, 3, 4, 5, 2, 1, 0};
+        int[] arr = {0,0, 3, 2, 1};
 
+        System.out.println(Arrays.toString(arr));
+
+        Solution solution = new Solution();
+        System.out.println(solution.validMountainArray(arr));
     }
 }
--- 941/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
+++ 941/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
@@ -2,6 +2,32 @@ package ru.te3ka.exercisesleetcode;
 
 public class Solution {
     public boolean validMountainArray(int[] arr) {
+        int i = 1;
+        boolean isUp = true;
+        boolean isDown = false;
+        if (i >= arr.length)
+            return false;
+        while (isUp) {
+            if (i + 1 >= arr.length)
+                return false;
+            if (arr[i] > arr[i - 1] && arr[i] < arr[i + 1])
+                i++;
+            else if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
+                isUp = false;
+                isDown = true;
+                i++;
+            }
+            else
+                return false;
+        }
 
+        while (isDown) {
+            if (i + 1 >= arr.length)
+                return true;
+            if (arr[i] < arr[i - 1] && arr[i] > arr[i + 1])
+                i++;
+            else return false;
+        }
+        return false;
     }
 }
