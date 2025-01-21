72b9c2c0a093e85e85bc44af57d6ed67decc26a1
Te3Ka <Mnement4813@yandex.ru>
2025-01-21 08:50:11 +0300

    solution 1299 start

---
+++ 1299/src/main/java/ru/te3ka/exercisesleetcode/Main1299.java
@@ -0,0 +1,15 @@
+package ru.te3ka.exercisesleetcode;
+
+import java.util.Arrays;
+
+public class Main1299 {
+    public static void main(String[] args) {
+        int[] arr = {17, 18, 5, 4, 6, 1};
+
+        System.out.println(Arrays.toString(arr));
+
+        Solution solution = new Solution();
+        solution.replaceElements(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
