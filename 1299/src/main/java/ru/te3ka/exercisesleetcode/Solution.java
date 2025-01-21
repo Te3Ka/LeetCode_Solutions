caff4f20b56c30463f07eb140cefbd1f39f3498e
Te3Ka <Mnement4813@yandex.ru>
2025-01-21 09:01:02 +0300

    solution 1299 re-complete

--- 1299/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
+++ 1299/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
@@ -2,16 +2,15 @@ package ru.te3ka.exercisesleetcode;
 
 public class Solution {
     public int[] replaceElements(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            if (i == arr.length - 1) {
-                arr[i] = -1;
-                return arr;
-            }
-
-            arr[i] = arr[i + 1];
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] < arr[j])
-                    arr[i] = arr[j];
+        int max = -1;
+        int temp = -1;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] > max) {
+                max = arr[i];
+                arr[i] = temp;
+                temp = max;
+            } else {
+                arr[i] = max;
             }
         }
         return arr;
