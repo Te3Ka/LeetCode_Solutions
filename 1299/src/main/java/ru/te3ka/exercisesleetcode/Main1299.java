eccb7c62c94f5e46a2d44d3e6d9366151d215e1a
Te3Ka <Mnement4813@yandex.ru>
2025-01-21 08:57:21 +0300

    solution 1299 complete

--- 1299/src/main/java/ru/te3ka/exercisesleetcode/Main1299.java
+++ 1299/src/main/java/ru/te3ka/exercisesleetcode/Main1299.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 
 public class Main1299 {
     public static void main(String[] args) {
-        int[] arr = {17, 18, 5, 4, 6, 1};
+//        int[] arr = {17, 18, 5, 4, 6, 1};
+        int[] arr = {400};
 
         System.out.println(Arrays.toString(arr));
 
--- 1299/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
+++ 1299/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
@@ -2,6 +2,18 @@ package ru.te3ka.exercisesleetcode;
 
 public class Solution {
     public int[] replaceElements(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (i == arr.length - 1) {
+                arr[i] = -1;
+                return arr;
+            }
+
+            arr[i] = arr[i + 1];
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] < arr[j])
+                    arr[i] = arr[j];
+            }
+        }
         return arr;
     }
 }
