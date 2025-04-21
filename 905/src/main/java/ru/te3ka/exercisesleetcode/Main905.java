ce69fcf30f94d207a7b823e142a05225e59f52ac
Te3Ka <Mnement4813@yandex.ru>
2025-04-21 17:09:07 +0300

    solution 905 complete-2

--- 905/src/main/java/ru/te3ka/exercisesleetcode/Main905.java
+++ 905/src/main/java/ru/te3ka/exercisesleetcode/Main905.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Main905 {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] nums = {3, 1, 2, 4, 5, 8, 7};
+        int[] nums = {2, 3, 1, 9, 9, 9, 9, 2, 4, 5, 8, 8, 8, 8, 8, 8, 7};
 
         System.out.println(Arrays.toString(nums));
         System.out.println(Arrays.toString(solution.sortArrayByParity(nums)));
--- 905/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
+++ 905/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
@@ -2,17 +2,13 @@ package ru.te3ka.exercisesleetcode;
 
 public class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        int firstNotParityNumIndex = 0;
+        int evenIndex = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 != 0 && nums[firstNotParityNumIndex] % 2 == 0) {
-                firstNotParityNumIndex = i;
-            }
-            if (nums[i] % 2 == 0 && nums[firstNotParityNumIndex] % 2 != 0) {
-                int temp = nums[firstNotParityNumIndex];
-                nums[firstNotParityNumIndex] = nums[i];
-                nums[i] = temp;
-
-                i = firstNotParityNumIndex;
+            if (nums[i] % 2 == 0) {
+                int temp = nums[i];
+                nums[i] = nums[evenIndex];
+                nums[evenIndex] = temp;
+                evenIndex++;
             }
         }
 
