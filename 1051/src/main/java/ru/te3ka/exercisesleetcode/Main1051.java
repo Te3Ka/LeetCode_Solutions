85fee8732226487c32e8a7120dd473cea4ccd718
Te3Ka <Mnement4813@yandex.ru>
2025-04-22 07:52:48 +0300

    solution 1051 complete

--- 1051/src/main/java/ru/te3ka/exercisesleetcode/Main1051.java
+++ 1051/src/main/java/ru/te3ka/exercisesleetcode/Main1051.java
@@ -1,9 +1,17 @@
 package ru.te3ka.exercisesleetcode;
 
 
+import java.util.Arrays;
+
 public class Main1051 {
     public static void main(String[] args) {
         Solution solution = new Solution();
+//        int[] heights = {1, 1, 4, 2, 1, 3};
+//        int[] heights = {5, 1, 2, 3, 4};
+        int[] heights = {1, 2, 3, 4, 5};
+
+        System.out.println(Arrays.toString(heights));
+        System.out.println(solution.heightChecker(heights));
 
     }
 }
--- 1051/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
+++ 1051/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
@@ -1,7 +1,18 @@
 package ru.te3ka.exercisesleetcode;
 
+import java.util.Arrays;
+
 public class Solution {
     public int heightChecker(int[] heights) {
-        return 0;
+        int[] expected = Arrays.stream(heights).sorted().toArray();
+        System.out.println(Arrays.toString(expected));
+
+        int errors = 0;
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] != expected[i])
+                errors++;
+        }
+
+        return errors;
     }
 }
