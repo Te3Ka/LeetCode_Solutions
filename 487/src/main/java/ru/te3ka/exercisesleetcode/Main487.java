021b242ed0831863ae2288282f43a2abadda8bd4
Te3Ka <Mnement4813@yandex.ru>
2025-04-23 08:03:21 +0300

    solution 947 complete

--- 487/src/main/java/ru/te3ka/exercisesleetcode/Main487.java
+++ 487/src/main/java/ru/te3ka/exercisesleetcode/Main487.java
@@ -1,7 +1,13 @@
 package ru.te3ka.exercisesleetcode;
 
+import java.util.Arrays;
+
 public class Main487 {
     public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1,0,1,1,0,1,1,1,0,0,1,1,1,1,1,0,1};
 
+        System.out.println(Arrays.toString(nums));
+        System.out.println(solution.findMaxConsecutiveOnes(nums));
     }
 }
