b4692efb920800de80a03005caf2c35252750e38
Te3Ka <Mnement4813@yandex.ru>
2025-04-27 14:55:18 +0300

    solution 448 complete

---
+++ 448/src/main/java/ru/te3ka/exercisesleetcode/Main448.java
@@ -0,0 +1,14 @@
+package ru.te3ka.exercisesleetcode;
+
+
+import java.util.Arrays;
+
+public class Main448 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {4,3,2,7,8,2,3,1};
+
+        System.out.println(Arrays.toString(nums));
+        System.out.println(solution.findDisappearedNumbers(nums));
+    }
+}
--- 448/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
+++ 448/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
@@ -1,9 +1,20 @@
 package ru.te3ka.exercisesleetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        return null;
+        List<Integer> returns = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            returns.add(i + 1);
+        }
+
+        for (int num : nums) {
+            if (returns.contains(num)) {
+                returns.remove(returns.indexOf(num));
+            }
+        }
+        return returns;
     }
 }
