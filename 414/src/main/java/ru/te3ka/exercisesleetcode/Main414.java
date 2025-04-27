8085f7b746e76ad381efee4cb569478e76137b58
Te3Ka <Mnement4813@yandex.ru>
2025-04-27 14:09:05 +0300

    solution 414 complete

--- 414/src/main/java/ru/te3ka/exercisesleetcode/Main414.java
+++ 414/src/main/java/ru/te3ka/exercisesleetcode/Main414.java
@@ -1,8 +1,14 @@
 package ru.te3ka.exercisesleetcode;
 
 
+import java.util.Arrays;
+
 public class Main414 {
     public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {4, 3,3,4, 2, 1, 5};
 
+        System.out.println(Arrays.toString(nums));
+        System.out.println(solution.thirdMax(nums));
     }
 }
--- 414/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
+++ 414/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
@@ -1,7 +1,34 @@
 package ru.te3ka.exercisesleetcode;
 
+import java.util.Arrays;
+
 public class Solution {
     public int thirdMax(int[] nums) {
-        return -1;
+        if (nums.length < 3)
+            return Arrays.stream(nums).max().getAsInt();
+
+        Integer firstMax = null;
+        Integer secondMax = null;
+        Integer thirdMax = null;
+
+        for (int num : nums) {
+            if (firstMax != null && num == firstMax ||
+                    secondMax != null && num == secondMax ||
+                    thirdMax != null && num == thirdMax)
+                continue;
+
+            if (firstMax == null || num > firstMax) {
+                thirdMax = secondMax;
+                secondMax = firstMax;
+                firstMax = num;
+            } else if (secondMax == null || num > secondMax) {
+                thirdMax = secondMax;
+                secondMax = num;
+            } else if (thirdMax == null || num > thirdMax) {
+                thirdMax = num;
+            }
+        }
+
+        return thirdMax == null ? firstMax : thirdMax;
     }
 }
