bae305f74e58105a9e3147b67b703cd6c48c52e2
Te3Ka <Mnement4813@yandex.ru>
2025-04-27 15:06:53 +0300

    solution 448 re-complete

--- 448/src/main/java/ru/te3ka/exercisesleetcode/Main448.java
+++ 448/src/main/java/ru/te3ka/exercisesleetcode/Main448.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class Main448 {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] nums = {4,3,2,7,8,2,3,1};
+        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
 
         System.out.println(Arrays.toString(nums));
         System.out.println(solution.findDisappearedNumbers(nums));
--- 448/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
+++ 448/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
@@ -1,20 +1,26 @@
 package ru.te3ka.exercisesleetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        List<Integer> returns = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            returns.add(i + 1);
+        for (int num : nums) {
+            int index = Math.abs(num) - 1;
+            if (nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
+            System.out.println(Arrays.toString(nums));
         }
 
-        for (int num : nums) {
-            if (returns.contains(num)) {
-                returns.remove(returns.indexOf(num));
+        List<Integer> results = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                results.add(i + 1);
             }
+            System.out.println("result = " + results);
         }
-        return returns;
+        return results;
     }
 }
