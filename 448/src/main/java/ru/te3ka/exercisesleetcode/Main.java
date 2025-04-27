9c294be217cbdeacbd060855b2e6ff1e596084d4
Te3Ka <Mnement4813@yandex.ru>
2025-04-27 14:18:55 +0300

    solution 448 start

---
+++ 448/src/main/java/ru/te3ka/exercisesleetcode/Main.java
@@ -0,0 +1,8 @@
+package ru.te3ka.exercisesleetcode;
+
+
+public class Main448 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 448/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
@@ -0,0 +1,9 @@
+package ru.te3ka.exercisesleetcode;
+
+import java.util.List;
+
+public class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        return null;
+    }
+}
