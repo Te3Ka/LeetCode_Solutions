57a54b584c019d34ad41a16c518c9e77d1726c94
Te3Ka <Mnement4813@yandex.ru>
2025-04-28 10:44:32 +0300

    solution 1 complete

--- 1/src/main/java/ru/te3ka/exercisesleetcode/Main1.java
+++ 1/src/main/java/ru/te3ka/exercisesleetcode/Main1.java
@@ -1,8 +1,16 @@
 package ru.te3ka.exercisesleetcode;
 
+import java.util.Arrays;
+
 public class Main1 {
     public static void main(String[] args) {
         Solution solution = new Solution();
+        int[] nums = {2,7,11,15};
+        int target = 18;
 
+        System.out.println(Arrays.toString(nums));
+        System.out.println("target = " + target);
+
+        System.out.println(Arrays.toString(solution.twoSum(nums, target)));
     }
 }
--- 1/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
+++ 1/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
@@ -2,6 +2,16 @@ package ru.te3ka.exercisesleetcode;
 
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        return null;
+        int[] returns = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = nums.length - 1; j > i; j--) {
+                if (nums[i] + nums[j] == target) {
+                    returns[0] = i;
+                    returns[1] = j;
+                    return returns;
+                }
+            }
+        }
+        return returns;
     }
 }
