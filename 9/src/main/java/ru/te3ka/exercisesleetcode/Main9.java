0774dec8256589928b97688e8141335766a4cb46
Te3Ka <Mnement4813@yandex.ru>
2025-04-29 09:48:33 +0300

    solution 9 complete

--- 9/src/main/java/ru/te3ka/exercisesleetcode/Main9.java
+++ 9/src/main/java/ru/te3ka/exercisesleetcode/Main9.java
@@ -4,5 +4,12 @@ package ru.te3ka.exercisesleetcode;
 public class Main9 {
     public static void main(String[] args) {
         Solution solution = new Solution();
+
+        System.out.println("x = " + 121 + " is palindrome? " + solution.isPalindrome(121));
+        System.out.println("x = " + 1001 + " is palindrome? " + solution.isPalindrome(1001));
+        System.out.println("x = " + -121 + " is palindrome? " + solution.isPalindrome(-121));
+        System.out.println("x = " + -12321 + " is palindrome? " + solution.isPalindrome(-12321));
+        System.out.println("x = " + 45654 + " is palindrome? " + solution.isPalindrome(45654));
+        System.out.println("x = " + 456540 + " is palindrome? " + solution.isPalindrome(456540));
     }
 }
--- 9/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
+++ 9/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
@@ -2,6 +2,21 @@ package ru.te3ka.exercisesleetcode;
 
 public class Solution {
     public boolean isPalindrome(int x) {
-        return false;
+        if (x < 0)
+            return false;
+        if (x == 0)
+            return true;
+        if (x % 10 == 0)
+            return false;
+
+        int temp = x;
+        int retX = 0;
+        while (temp > 0) {
+            retX += temp % 10;
+            if (temp >= 10)
+                retX *= 10;
+            temp /= 10;
+        }
+        return retX == x;
     }
 }
