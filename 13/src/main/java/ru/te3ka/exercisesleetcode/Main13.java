38678399408da02904ce63844a2ff32de9c75cae
Te3Ka <Mnement4813@yandex.ru>
2025-04-30 10:36:07 +0300

    solution 13 complete

--- 13/src/main/java/ru/te3ka/exercisesleetcode/Main13.java
+++ 13/src/main/java/ru/te3ka/exercisesleetcode/Main13.java
@@ -3,6 +3,10 @@ package ru.te3ka.exercisesleetcode;
 public class Main13 {
     public static void main(String[] args) {
         Solution solution = new Solution();
+        String s3 = "III";
+        String s1994 = "MCMXCIV";
 
+        System.out.println(s1994);
+        System.out.println(solution.romanToInt(s1994));
     }
 }
--- 13/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
+++ 13/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
@@ -2,6 +2,76 @@ package ru.te3ka.exercisesleetcode;
 
 public class Solution {
     public int romanToInt(String s) {
-        return 0;
+        int res = 0;
+
+        char[] romNumbers = s.toCharArray();
+        for (int i = 0; i < romNumbers.length; i++) {
+            if (i == romNumbers.length - 1) {
+                if (romNumbers[i] == 'I')
+                    res += 1;
+                if (romNumbers[i] == 'V')
+                    res += 5;
+                if (romNumbers[i] == 'X')
+                    res += 10;
+                if (romNumbers[i] == 'L')
+                    res += 50;
+                if (romNumbers[i] == 'C')
+                    res += 100;
+                if (romNumbers[i] == 'D')
+                    res += 500;
+                if (romNumbers[i] == 'M')
+                    res += 1000;
+                break;
+            } else {
+                if (romNumbers[i] == 'C') {
+                    if (romNumbers[i + 1] == 'D') {
+                        res += 400;
+                        i++;
+                        continue;
+                    }
+                    if (romNumbers[i + 1] == 'M') {
+                        res += 900;
+                        i++;
+                        continue;
+                    }
+                    res += 100;
+                }
+                if (romNumbers[i] == 'X') {
+                    if (romNumbers[i + 1] == 'L') {
+                        res += 40;
+                        i++;
+                        continue;
+                    }
+                    if (romNumbers[i + 1] == 'C') {
+                        res += 90;
+                        i++;
+                        continue;
+                    }
+                    res += 10;
+                }
+                if (romNumbers[i] == 'I') {
+                    if (romNumbers[i + 1] == 'V') {
+                        res += 4;
+                        i++;
+                        continue;
+                    }
+                    if (romNumbers[i + 1] == 'X') {
+                        res += 9;
+                        i++;
+                        continue;
+                    }
+                    res += 1;
+                }
+                if (romNumbers[i] == 'V')
+                    res += 5;
+                if (romNumbers[i] == 'L')
+                    res += 50;
+                if (romNumbers[i] == 'D')
+                    res += 500;
+                if (romNumbers[i] == 'M')
+                    res += 1000;
+            }
+        }
+        return res;
     }
 }
