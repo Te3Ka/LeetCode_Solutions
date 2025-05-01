6f8cadd141968dab7a93501df6fcf3948d09a404
Te3Ka <Mnement4813@yandex.ru>
2025-05-01 14:02:38 +0300

    solution 14 complete

--- 14/src/main/java/ru/te3ka/exercisesleetcode/Main14.java
+++ 14/src/main/java/ru/te3ka/exercisesleetcode/Main14.java
@@ -1,7 +1,15 @@
 package ru.te3ka.exercisesleetcode;
 
+import java.util.Arrays;
+
 public class Main14 {
     public static void main(String[] args) {
+        Solution solution = new Solution();
+//        String[] strs = {"flower", "flow", "flight"};
+//        String[] strs = {"flower", "flow", "flowwow"};
+        String[] strs = {"bands", "bandana", "banana", "bang"};
 
+        System.out.println(Arrays.toString(strs));
+        System.out.println(solution.longestCommonPrefix(strs));
     }
 }
--- 14/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
+++ 14/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
@@ -2,6 +2,24 @@ package ru.te3ka.exercisesleetcode;
 
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
-        return "";
+        StringBuilder lcp = new StringBuilder();
+
+        int minLen = Integer.MAX_VALUE;
+        for (String str : strs) {
+            if (minLen > str.length())
+                minLen = str.length();
+        }
+
+        for (int i = 0; i < minLen; i++) {
+            char _char = strs[0].charAt(i);
+            for (String str : strs) {
+                if (str.charAt(i) != _char) {
+                    return lcp.toString();
+                }
+            }
+            lcp.append(_char);
+        }
+
+        return lcp.toString();
     }
 }
