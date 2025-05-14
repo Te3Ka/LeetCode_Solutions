359f6e01e704d71e1198cdb02e272e7affdb6bb8
Te3Ka <Mnement4813@yandex.ru>
2025-05-14 08:04:56 +0300

    solution 20 complete

--- 20/src/main/java/ru/te3ka/exercisesleetcode/Main20.java
+++ 20/src/main/java/ru/te3ka/exercisesleetcode/Main20.java
@@ -3,6 +3,10 @@ package ru.te3ka.exercisesleetcode;
 public class Main20 {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String s = "()[]{}";
+//        String s = "()[]{}";
+        String s = "()([]{})";
+
+        System.out.println(s);
+        System.out.println(solution.isValid(s));
     }
 }
--- 20/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
+++ 20/src/main/java/ru/te3ka/exercisesleetcode/Solution.java
@@ -2,6 +2,25 @@ package ru.te3ka.exercisesleetcode;
 
 public class Solution {
     public boolean isValid(String s) {
-        return false;
+        char[] arrParentheses = new char[s.length()];
+        int top = -1;
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                arrParentheses[++top] = ch;
+            }
+            else {
+                if (top == -1)
+                    return false;
+                char last = arrParentheses[top--];
+                if ((ch == ')' && last != '(') ||
+                        (ch == '}' && last != '{') ||
+                        (ch == ']' && last != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return top == -1;
     }
 }
